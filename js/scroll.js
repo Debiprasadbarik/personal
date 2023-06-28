20a040469565277f509a96f91e8b9bb7d7a42392
Debi Prasad Barik <debiprasadbarikm@gmail.com>
2023-06-28 11:25:37 +0530

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,36 @@
+//toggle icon navbar
+let menuIcon =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+//scroll section
+let section =document.querySelectorAll('section');
+let navlinks =document.querySelectorAll('header nav a');
+window.onscroll =()=>{
+    section.forEach(sec =>{
+        let top= window.scrollY;
+        let offset =sec.offsetTop -100;
+        let height =sec.offsetHeight;
+        let id =sec.getAttribute('id');
+
+        if(top >=offset && top <offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        }
+    });
+    //sticky header
+    let header =document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY>100);
+    //remove toggle icon and navbar when click navbar links(scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
